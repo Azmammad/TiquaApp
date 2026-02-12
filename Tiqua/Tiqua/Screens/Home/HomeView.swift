758bf88b88a513f8615b5331d579bb595924740f
Azmammad <azmammadjabrayilov@gmail.com>
2026-02-13 01:02:52 +0400

    created empty HomeView

---
+++ Tiqua/Tiqua/Screens/Home/HomeView.swift
@@ -0,0 +1,13 @@
+//
+//  HomeView.swift
+//  Tiqua
+//
+//  Created by Əzi Cəbrayılov on 11.02.26.
+//
+import SwiftUI
+
+struct HomeView:View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
